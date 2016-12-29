542538a47db027987f496f3af2124acb01a2a5e9
Jerry Chang <therealjerrycan@gmail.com>
2016-12-28 18:23:30 -0800

    remove src

--- src/client.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import AppRoutes from './routes/routes';
-
-const app = document.getElementById('app');
-
-ReactDOM.render(<AppRoutes />, app);
